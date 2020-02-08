7ba0a2ccd7d1a0fb731338aaf9e1f34cb0309d78
swsong <songaal@gmail.com>
2020-02-08 20:20:06 +0900

    ..

--- bots/week_breakout.py
+++ bots/week_breakout.py
@@ -99,7 +99,8 @@ if __name__ == '__main__':
                    )
 
     algo = WeekBreakout(base='BTC', quote='USDT', floor_decimals=3, init_capital=1000, max_budget=1000000,
-                        week_start=Yoil.MON, hour_start=0, long_rate=0.4, short_rate=0.4, buy_unit=0.01, buy_delay=1)
+                        week_start=Yoil.MON, hour_start=0, long_rate=0.4, short_rate=0.4, buy_unit=0.01, buy_delay=1,
+                        commission_rate=0.1)
 
     # asyncio.run(test_bot.run(algo))
     asyncio.run(real_bot.run(algo))
